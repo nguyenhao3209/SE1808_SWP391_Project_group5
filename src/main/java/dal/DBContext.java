4a41a7d2a0d70a6d5f1d14850056b727f21c6d8c
nguyenhao3209 <haontce180451@fpt.edu.vn>
2025-03-18 12:39:11 +0700

    fix: connect database

--- src/main/java/dal/DBContext.java
+++ src/main/java/dal/DBContext.java
@@ -20,7 +20,7 @@ public class DBContext {
     public DBContext() {
         try {
            String user = "sa";
-           String pass = "123";
+           String pass = "123456";
            String url = "jdbc:sqlserver://localhost:1433;databaseName=SE1808_SWP391_Group5;trustServerCertificate=true";
 
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
